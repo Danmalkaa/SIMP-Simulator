abbea49b4227a5f50f9d2133a2694bd7a21083b7
Danmalkaa <45342726+Danmalkaa@users.noreply.github.com>
2020-12-30 21:38:40 +0200

    Add files via upload
    Updated all set - good imemin for the fib.asm test

--- Assembler.c
+++ Assembler.c
@@ -10,7 +10,124 @@
 #include <string.h>
 #define MAX_LEN 256
 #define MAXLABELSIZE 10
+#define MAXSIZE 80
 
+typedef struct LABEL
+{
+    char LABELNAME[MAXLABELSIZE];
+    int labeladdress;
+    struct LABEL* next;
+}LABEL;
+
+LABEL* create_label(char label_name[MAXLABELSIZE], int label_address) { //creat new label
+    LABEL *new_label;
+
+    new_label = (LABEL*)malloc(sizeof(LABEL)); //allocate memmory for new label
+    if (new_label == NULL) {
+        printf("Fail to allocate memmory");
+        return NULL;
+    }
+    strcpy(new_label->LABELNAME, label_name); //give label name
+    new_label->labeladdress = label_address; //number of row
+    new_label->next = NULL;
+
+    return(new_label);
+}
+
+LABEL* first_read(FILE* rfile) {
+    char LINE[MAXSIZE]; // a place to put the line and labels
+    char strs[6][MAXLABELSIZE]; // a list for the registers name
+    int i, l, pc_counter, elements;
+    LABEL* label_list = NULL;
+    LABEL *current = NULL, *next_label;
+    char *ptr, *token;
+    const char clean[10] = " :,.-\t\n\r";
+
+    pc_counter = 0;
+    while (feof(rfile) == 0) { //read next line untill the end of the file
+        fgets(LINE, MAXSIZE, rfile);
+        ptr = strchr(LINE, '#'); //clear all string after '#'
+        if (ptr != NULL) {
+            *ptr = '\0';
+        }
+        token = strtok(LINE, clean); //break the string into tokens
+        elements = 0;
+        for (i = 0; i < 6; i++) { // insert the tokens into the list, if there is nothing left insert NULL
+            if (token == NULL) {
+                strs[i][0] = NULL;
+                token = strtok(NULL, clean);
+            }
+            else {
+                strcpy(strs[i], token);
+                token = strtok(NULL, clean);
+                elements++;
+            }
+        }
+        if (elements == 1) {//there is only label in this row
+            if (label_list == NULL) { //insert the first label
+                label_list = create_label(strs[0], pc_counter);
+                current = label_list;
+            }
+            else {
+                next_label = create_label(strs[0], pc_counter);//insert new label
+                current->next = next_label;
+                current = current->next;
+            }
+        }
+        else if (elements == 6) { //this is a row with label and command
+            if (label_list == NULL) { //insert the first label
+                label_list = create_label(strs[0], pc_counter);
+                current = label_list;
+            }
+            else {
+                next_label = create_label(strs[0], pc_counter);//insert new label
+                current->next = next_label;
+                current = current->next;
+            }
+            pc_counter += 2; //progress the pc counter by 2
+        }
+        else if (elements == 0)
+            continue;
+        else {
+            l = 0;
+            for (i = 0; i < 6; i++) {
+                if (strcmp("$imm", strs[i]) == 0) {
+                    pc_counter += 2;
+                    l = 1;
+                    break;
+                }
+            }
+            if (l == 0)
+                pc_counter += 1;
+        }
+    }
+
+    return (label_list);
+}
+
+int searchforlabel(LABEL *label_list, char *label_name)
+{
+    int result = 0, found;
+    LABEL *current;
+
+    current = label_list;
+    found = 0;
+    while (current != NULL)
+    {
+        if (strcmp(current->LABELNAME, label_name) == 0) {
+            found = 1;
+            result = current->labeladdress;
+            break;
+        }
+        else {
+            current = current->next;
+        }
+    }
+    if (found == 0)
+        return (-1);
+    else
+        return(result);
+}
 
 void slice(const char *str, char *result, size_t start, size_t end)
 {
@@ -70,11 +187,11 @@ dict register_to_hexa_dict [] =
     { "$a1", "4" },
     { "$t0", "5" },
     { "$t1", "6" },
-    { "$t2 ", "7" },
+    { "$t2", "7" },
     { "$t3", "8" },
     { "$s0", "9" },
-    { "$s1 ", "A" },
-    { "$s2 ", "B" },
+    { "$s1", "A" },
+    { "$s2", "B" },
     { "$gp", "C" },
     { "$sp", "D" },
     { "$fp", "E"},
@@ -146,24 +263,29 @@ char* getValue(char * str, int mode)
 
 int main(void)
 {
+    
 //    char *string = ReadFile("/Users/danmalka/Documents/לימודים/שנה\ ג/סמסטר\ א/מבנה\ המחשב/פרויקט/SIMP\ Assembler/fib.asm");
-    FILE* fp;
+    // Think if we need to free malloc allocated memory
+    FILE *fp, *fw_imemin; // check if needed as "a" mode or "w" mode
     fp = fopen("/Users/danmalka/Documents/לימודים/שנה\ ג/סמסטר\ א/מבנה\ המחשב/פרויקט/SIMP\ Assembler/fib.asm", "r");
     if (fp == NULL) {
       perror("Failed: ");
       return 1;
     }
-
+    LABEL *label_list = first_read(fp);
+    rewind(fp);
     char buffer[MAX_LEN];
     // -1 to allow room for NULL terminator for really long string
+    fw_imemin = fopen("/Users/danmalka/Documents/לימודים/שנה\ ג/סמסטר\ א/מבנה\ המחשב/פרויקט/SIMP\ Assembler/imemin.txt", "w");
     while (fgets(buffer, MAX_LEN - 1, fp))
     {
+        int is_imm=0;
         // Remove trailing newline
         buffer[strcspn(buffer, "\n")] = 0; // sets the buffer to 0 after the index for end of line
         char * pch;
         int k = 0;
         char* parameters[5]; // init the 5 commands array in the line
-        pch = strtok (buffer," ,\t");
+        pch = strtok (buffer," ,\t:");
         while (pch != NULL)
         {
             if (*pch == '#') // reached comments
@@ -171,19 +293,41 @@ int main(void)
               pch = NULL; // to get out of loop
               continue;
             }
-            parameters[k++] = pch; //assigns the command in the 5 commands 1-line array
+            parameters[k++] = pch;//assigns the command in the 5 commands 1-line array
+            if (strcmp("$imm", pch) == 0)
+                is_imm = 1;
             printf ("%s\n",pch); // just for debug
             pch = strtok (NULL, " ,\t"); // gets next token
         }
-        char * to_imemin[5];
+        int imm,label_line;
+        unsigned int imm_5;
+        label_line = searchforlabel(label_list, parameters[0]);
+        if (label_line!=-1) // check if it is a label line
+            continue;
+        imm = searchforlabel(label_list, parameters[4]); // check if there is a label in the last token
+        if (imm ==-1) // no label
+        {
+            imm = strtol(parameters[4], NULL, 10);
+            imm_5 = (unsigned int)(imm & 0xFFFFF);
+        }
+        else
+        {
+            imm_5 = (unsigned int)(imm & 0xFFFFF);
+        }
         char*  opcode;
-        char *rd,*rs,*rt,*to_print;
+        char *rd,*rs,*rt;
+
+        if (strcmp(".word", parameters[0]) == 0) // check if it is a .word command
+            continue;
         opcode = getValue(parameters[0], 1);
         rd = getValue(parameters[1], 2);
         rs = getValue(parameters[2], 2);
         rt = getValue(parameters[3], 2);
-        printf("%s%s%s%s\n",opcode,rd,rs,rt); // change to fprintf
-        // add here - if == &imm (or rt== "1") another printf \n
+        fprintf(fw_imemin,"%s%s%s%s\n",opcode,rd,rs,rt); // change to fprintf
+        if (is_imm)
+            fprintf(fw_imemin,"%05X\n",imm_5);
     }
+    fclose(fp);
+    fclose(fw_imemin);
     return 0;
 }
